3ebc0b35b175bfa5cbc453f05983eda71bb4cf1c
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2017-01-24 19:12:20 +1100

    Login and Register complete

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -11,11 +11,17 @@ package com.parse.starter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.parse.LogInCallback;
 import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -33,5 +39,25 @@ public class MainActivity extends AppCompatActivity {
     public void SignUpLink(View view) {
         Intent signUpIntent = new Intent(getApplicationContext(), SignUpActivity.class);
         startActivity(signUpIntent);
+    }
+
+    public void Login(View view) {
+        EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        EditText etPassword = (EditText) findViewById(R.id.etPassword);
+
+        ParseUser.logInInBackground(etUsername.getText().toString(), etPassword.getText().toString(), new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e == null) {
+                    Log.i("Info", "Logged In");
+                }
+                else {
+                    Toast.makeText(
+                            getApplicationContext(),
+                            e.getMessage(),
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
--- ParseStarterProject/src/main/java/com/parse/starter/SignUpActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/SignUpActivity.java
@@ -6,7 +6,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+import static android.R.attr.name;
+import static com.parse.starter.R.id.etUsername;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -23,4 +34,42 @@ public class SignUpActivity extends AppCompatActivity {
         startActivity(loginIntent);
     }
 
+    public void SignUp(final View view) {
+        EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        EditText etPassword2 = (EditText) findViewById(R.id.etPassword2);
+
+        if (etPassword.getText().toString().equals(etPassword2.getText().toString())) {
+            ParseUser parseUser = new ParseUser();
+            parseUser.setUsername(etUsername.getText().toString());
+            parseUser.setPassword(etPassword.getText().toString());
+            parseUser.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null) {
+                        Toast.makeText(
+                                getApplicationContext(),
+                                "Account Created",
+                                Toast.LENGTH_SHORT
+                        );
+                        LoginLink(view);
+                    }
+                    else {
+                        Toast.makeText(
+                                getApplicationContext(),
+                                e.getMessage(),
+                                Toast.LENGTH_SHORT
+                        );
+                    }
+                }
+            });
+        }
+        else {
+            Toast.makeText(
+                    getApplicationContext(),
+                    "Passwords do not match",
+                    Toast.LENGTH_SHORT
+            ).show();
+        }
+    }
 }
