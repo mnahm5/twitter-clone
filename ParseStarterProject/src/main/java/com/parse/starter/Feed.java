2837165312c9bc28f5f006f137c36009823d1d40
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2017-01-24 21:41:09 +1100

    finished feed

--- ParseStarterProject/src/main/java/com/parse/starter/Feed.java
+++ ParseStarterProject/src/main/java/com/parse/starter/Feed.java
@@ -6,6 +6,12 @@ import android.support.v7.widget.ListViewCompat;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -17,24 +23,39 @@ public class Feed extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+        setTitle("Your Feed");
 
-        ListView lvFeed = (ListView) findViewById(R.id.lvFeed);
-        List<Map<String, String>> tweetData = new ArrayList<Map<String, String>>();
+        final ListView lvFeed = (ListView) findViewById(R.id.lvFeed);
 
-        for (int i  = 0; i < 5; i++) {
-            Map<String, String> tweetInfo = new HashMap<String, String>();
-            tweetInfo.put("content", "Tweet Content " + Integer.toString(i));
-            tweetInfo.put("username", "User" + Integer.toString(i));
-            tweetData.add(tweetInfo);
-        }
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Tweet");
+        query.whereContainedIn("username", ParseUser.getCurrentUser().getList("following"));
+        query.orderByDescending("createdAt");
+        query.setLimit(20);
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if (e == null) {
+                    if (objects.size() > 0) {
+                        List<Map<String, String>> tweetData = new ArrayList<Map<String, String>>();
+                        for (ParseObject tweet: objects) {
+                            Map<String, String> tweetInfo = new HashMap<String, String>();
+                            tweetInfo.put("content", tweet.getString("tweet"));
+                            tweetInfo.put("username", tweet.getString("username"));
+                            tweetData.add(tweetInfo);
+                        }
+                        SimpleAdapter simpleAdapter = new SimpleAdapter(
+                                Feed.this,
+                                tweetData,
+                                android.R.layout.simple_expandable_list_item_2,
+                                new String[] {"content", "username"},
+                                new int[] {android.R.id.text1, android.R.id.text2});
 
-        SimpleAdapter simpleAdapter = new SimpleAdapter(
-                this,
-                tweetData,
-                android.R.layout.simple_expandable_list_item_2,
-                new String[] {"content", "username"},
-                new int[] {android.R.id.text1, android.R.id.text2});
+                        lvFeed.setAdapter(simpleAdapter);
+                    }
+                }
+            }
+        });
 
-        lvFeed.setAdapter(simpleAdapter);
+
     }
 }
